7e1edad8f1ff969ce496f70df56e0f0015a52ec6
aleksandra.vidovic@student.htl-rankweil.at <aleksandra.vidovic@student.htl-rankweil.at>
2026-03-04 10:44:34 +0000

    Created Backend and Startseite.RowTemplate1; Edited Startseite

--- client_code/Startseite/RowTemplate1/__init__.py
+++ client_code/Startseite/RowTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import StartseiteTemplate
+from ._anvil_designer import RowTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Startseite(StartseiteTemplate):
+class RowTemplate1(RowTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
