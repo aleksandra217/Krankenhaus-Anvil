2835b43e4a107660b2a63c13e4f4e5e61eb6544c
aleksandra.vidovic@student.htl-rankweil.at <aleksandra.vidovic@student.htl-rankweil.at>
2026-03-05 11:27:59 +0000

    Edited Startseite and Backend

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -12,3 +12,10 @@ class Startseite(StartseiteTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("Drop_Down_Menu_Abteilungen", "change")
+  def Drop_Down_Menu_Abteilungen_change(self, **event_args):
+    """This method is called when an item is selected"""
+    if self.Drop_Down_Menu_Abteilungen.selected_value is not None:
+      self.data_grid_1.visible = True;
+    
--- server_code/Backend.py
+++ server_code/Backend.py
@@ -20,7 +20,17 @@ import sqlite3
 
 @anvil.server.callable
 def query_database(query: str):
-  with sqlite3.connect(data_files["krankenhaus"]) as conn:
+  with sqlite3.connect(data_files["krankenhaus.db"]) as conn:
     cur = conn.cursor()
     result = cur.execute(query).fetchall()
   return result
+
+@anvil.server.callable
+def query_database_dict(query: str):
+  with sqlite3.connect(data_files["krankenhaus.db"]) as conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+  return [dict(row) for row in result]
+
+
